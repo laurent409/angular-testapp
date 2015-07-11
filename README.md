8949adee118589e840f10afb5663156065ad5f4b
Laurent Philibert <philibert.laurent@gmail.com>
2015-07-11 12:51:25 +0200

    todo-list trying to fix all todo-list deletion

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # angular-testapp
+
+Functionalities : 
+Todo list -> fix all deletions when button "ALL" is checked
+Chat -> development
+
---
+++ js/todoList.js
@@ -0,0 +1,84 @@
+//js/todoList.js
+'use strict';
+
+
+/**
+ * 	Application declaration : demoApp
+ */
+var demoApp = angular.module('demoApp', [
+		
+	//modules list dependencies(declaration)
+	'todoList'
+]);
+
+
+/**
+ * 	Module declaration : todoList
+ */
+var todoList = angular.module('todoList', []); //no dependencies
+
+
+/**
+ * 	Controller declaration : todoCtrl
+ */
+ var todoCtrl = todoList.controller('todoCtrl', ['$scope',
+
+ 	function($scope){
+
+ 		//Empty tab declaration for $scope.todos
+ 		var todos = $scope.todos = [];
+
+
+		/**
+		 * 	Adding a todo
+		 */
+		$scope.addTodo = function(){
+
+			//Getting new todo
+			//trim() is for delete unwanted spaces before and after word
+			var newTodo = $scope.newTodo.trim();
+			console.log(newTodo);
+			//Checking if the new todo is empty
+			if (!newTodo.length)
+				return;
+			
+			//Pushing the new todo into todos var (for more information, look the controller declaration part)
+			todos.push({
+				title: newTodo, //the name of the todo
+				completed: false //the new todo isn't checked
+			});
+			
+			//Putting the $scope.newTodo to empty for adding a new todo soon
+			$scope.newTodo = '';
+		};
+
+		/**
+		 *	Removing a todo
+		 */
+		 $scope.removeTodo = function(todo){
+		 	todos.splice(todos.indexOf(todo),1);
+		 };
+
+		/**
+		 *	Checking or not all todos
+		 */
+		 $scope.markAll = function(completed){
+		 	todos.forEach(function(todo){
+                todo.completed = completed;
+		 	});
+		 };
+
+
+		/**
+		 *	Removing all checked todos
+		 */
+		 $scope.clearCompleteTodos = function(){
+		 	$scope.todos = todos = todos.filter(function(todo){
+		 		return !todos.completed;
+		 	});
+		 }; 		
+ 	}
+
+]);
+
+
